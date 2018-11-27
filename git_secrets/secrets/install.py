396576376d50c1407affb9ef6f29c02683433666
halprin <halprin@users.noreply.github.com>
2018-11-27 14:33:40 -0500

    Clean-up unused code

--- git_secrets/secrets/install.py
+++ git_secrets/secrets/install.py
@@ -5,7 +5,7 @@ import os
 import stat
 
 
-def install(force: bool=False):
+def install():
     hooks_directory = git.get_hooks_directory()
     if hooks_directory is None:
         cli_library.fail_execution(3, 'There is no hook directory')
@@ -16,11 +16,8 @@ def install(force: bool=False):
     if path.isdir(pre_commit_directory):
         _create_hook('git-secrets.sh', pre_commit_directory)
     elif path.exists(pre_commit_hook):
-        if force:
-            _install_multihooks(hooks_directory)
-        else:
-            cli_library.fail_execution(4, 'The pre-commit hook already exists.  To force installation with multiple'
-                                          'hooks, specify --force')
+        cli_library.fail_execution(4, "The pre-commit hook already exists.  You can manually scan by running 'git "
+                                      "secrets scan'")
     else:
         _create_hook('pre-commit', hooks_directory)
 
@@ -32,11 +29,3 @@ def _create_hook(name: str, directory_path: str):
         hook.writelines(['#!/usr/bin/env bash\n', 'git secrets scan\n'])
 
     os.chmod(hook_path, os.stat(hook_path).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-
-
-def _install_multihooks(hooks_directory: str):
-    pass
-    # download https://raw.githubusercontent.com/halprin/git-multihooks/master/git-multihooks into /usr/local/bin/
-    # invoke _create_hook in /tmp/
-    # move existing pre-commit into /tmp/ and call it old_pre-commit
-    # run git multihooks add pre-commit /tmp/*newly created hook* and *moved existing pre-commit hook*
