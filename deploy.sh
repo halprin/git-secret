daac85fefeaac5847869a305da22455fcf7552d9
halprin <halprin@users.noreply.github.com>
2018-11-28 10:51:08 -0500

    Set-up initial CI files

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Delete previous distribution archives
 rm -rf ./dist/
 rm -rf ./build/
-rm -rf ./git_secrets.egg-info/
+rm -rf ./qpp_git_secrets.egg-info/
 
 # Generate distribution archives
 python setup.py sdist bdist_wheel
